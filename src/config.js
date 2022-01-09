6fa643f8d6f6a271bd493981c1ede872761bcf46
rng70 <1705070@ugrad.cse.buet.ac.bd>
2022-01-10 01:25:34 +0600

    config files added and so sc pp rtg

---
+++ src/config.js
@@ -0,0 +1,28 @@
+module.export = {
+    navLinks: [
+        {
+            name: 'Channel',
+            url: '/channel',
+        },
+        {
+            name: 'Content',
+            url: '/content',
+        },
+        {
+            name: 'Collaboration',
+            url: '/collaboration',
+        },
+        {
+            name: 'Library',
+            url: '/library',
+        },
+        {
+            name: 'Forum',
+            url: '/forum',
+        },
+        {
+            name: 'UserProfileIcon',
+            url: '/userprofile/slug',
+        }
+    ],
+}
