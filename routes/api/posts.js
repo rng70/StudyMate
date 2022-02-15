804e457bc94c6636ed6650ed01357a9d8f37eca6
rng70 <1705070@ugrad.cse.buet.ac.bd>
2022-02-15 16:30:43 +0600

    get all post and single post done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 /**
- * @route   GET api/posts
+ * @route   POST api/posts
  * @desc    Create a post
  * @access  Private
  */ 
@@ -41,7 +41,43 @@ router.post('/',
             console.error(err.message);
             return res.status(500).send('Server Error');
         }
-    });
+});
 
+/**
+ * @route   GET api/posts
+ * @desc    Get all post
+ * @access  Conditionaloy Public
+ */ 
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @route   GET api/posts
+ * @desc    Get all post
+ * @access  Conditionaloy Public
+ */ 
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
