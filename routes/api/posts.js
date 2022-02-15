8dabe15c3cb0628cdbb49a1e5fb0696f413cc054
rng70 <1705070@ugrad.cse.buet.ac.bd>
2022-02-15 16:38:34 +0600

    delete post added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -59,7 +59,7 @@ router.get('/', auth, async (req, res) => {
 });
 
 /**
- * @route   GET api/posts
+ * @route   GET api/posts/:id
  * @desc    Get all post
  * @access  Conditionaloy Public
  */ 
@@ -71,6 +71,35 @@ router.get('/:id', auth, async (req, res) => {
             return res.status(404).json({ msg: 'Post not found' });
         }
         res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @route   DELETE api/posts/:id
+ * @desc    Delete post
+ * @access  Private
+ */ 
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        /* Check user */
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+        res.json({msg: 'Post removed'});
     } catch (err) {
         console.error(err.message);
         if (err.kind === 'ObjectId') {
